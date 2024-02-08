54653e6526421e45a8a2c42699f06044899bd270
John Farina <johnfarina8@gmail.com>
2024-02-07 20:07:52 -0500

    Update information and add images (#5)

--- src/App/MainPage/cards/ExperienceCard/CompanyLink/CompanyLink.tsx
+++ src/App/MainPage/cards/ExperienceCard/CompanyLink/CompanyLink.tsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
 import './CompanyLink.scss';
 
 interface Props {
     title: string;
     visibleLink: string;
     link: string;
+    company: `swivvel` | `bedroomProducer`;
 }
 
-const CompanyLink = ({ title, visibleLink, link }: Props) => {
+const CompanyLink = ({ title, visibleLink, link, company }: Props) => {
+    const [hoverOverBtn, setHoverOverBtn] = useState(false);
+
+    let companyName;
+    if (company === "swivvel") {
+        companyName = "Swivvel";
+    } else if (company === "bedroomProducer") {
+        companyName = "Bedroom Producer";
+    } else {
+        const exhaustiveCheck: never = company;
+        return exhaustiveCheck;
+    }
+
+    const atSignClass = hoverOverBtn ? `hovering` : ``;
+
+
     return (
         <p className="paragraph">
-            {title} -{" "}
+            {title} <span className={`atSign ${atSignClass} ${company}`}>@</span> {companyName} -{" "}
             <a
-                className="listLink"
+                className={`listLink ${company}`}
+                onMouseEnter={() => setHoverOverBtn(true)}
+                onMouseLeave={() => setHoverOverBtn(false)}
                 target="_blank"
                 rel="noreferrer"
                 href={link}
